b9c54c4b62dc427d4549c7b27006a82546698f11
egor_pilat <Egor_Pilat@epam.com>
2020-05-24 21:03:05 +0300

    add argparse_argument in main

--- main.py
+++ main.py
@@ -27,7 +27,43 @@ if __name__ == '__main__':
     parser.add_argument('--weights')
     parser.add_argument('--save_weights')
     parser.add_argument('--folder_path')
-
+    parser.add_argument('--trained_model', default='weights/craft_mlt_25k.pth', type=str, help='pretrained model')
+    parser.add_argument('--text_threshold', default=0.7, type=float, help='text confidence threshold')
+    parser.add_argument('--low_text', default=0.4, type=float, help='text low-bound score')
+    parser.add_argument('--link_threshold', default=0.4, type=float, help='link confidence threshold')
+    parser.add_argument('--cuda', default=False, type=str2bool, help='Use cuda for inference')
+    parser.add_argument('--canvas_size', default=1280, type=int, help='image size for inference')
+    parser.add_argument('--mag_ratio', default=1.5, type=float, help='image magnification ratio')
+    parser.add_argument('--poly', default=False, action='store_true', help='enable polygon type')
+    parser.add_argument('--show_time', default=False, action='store_true', help='show processing time')
+    parser.add_argument('--test_folder', default='/data/', type=str, help='folder path to input images')
+    parser.add_argument('--refine', default=False, action='store_true', help='enable link refiner')
+    parser.add_argument('--refiner_model', default='weights/craft_refiner_CTW1500.pth', type=str,
+                        help='pretrained refiner model')
+    parser.add_argument('--image_folder', default='lol', help='path to image_folder which contains text images')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
+    parser.add_argument('--saved_model', help="path to saved_model to evaluation",
+                        default='weights/TPS-ResNet-BiLSTM-Attn.pth')
+    """ Data processing """
+    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
+    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
+    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
+    parser.add_argument('--rgb', action='store_true', help='use rgb input')
+    parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
+    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
+    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
+    """ Model Architecture """
+    parser.add_argument('--Transformation', type=str, help='Transformation stage. None|TPS', default='TPS')
+    parser.add_argument('--FeatureExtraction', type=str, help='FeatureExtraction stage. VGG|RCNN|ResNet',
+                        default='ResNet')
+    parser.add_argument('--SequenceModeling', type=str, help='SequenceModeling stage. None|BiLSTM', default='BiLSTM')
+    parser.add_argument('--Prediction', type=str, help='Prediction stage. CTC|Attn', default='Attn')
+    parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
+    parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
+    parser.add_argument('--output_channel', type=int, default=512,
+                        help='the number of output channel of Feature extractor')
+    parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
 
     args = parser.parse_args()
 
@@ -42,4 +78,4 @@ if __name__ == '__main__':
     if args.command == 'visualize':
         if args.network == 'imagenet':
             visualize_model(args.device, args.weights, 6)
-    
+
---
+++ ocr_tools/CRAFT-pytorch/test.py
@@ -0,0 +1,197 @@
+"""  
+Copyright (c) 2019-present NAVER Corp.
+MIT License
+"""
+
+# -*- coding: utf-8 -*-
+import sys
+import os
+import time
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+
+from PIL import Image
+
+import cv2
+from skimage import io
+import numpy as np
+import pandas as pd
+import craft_utils
+import imgproc
+import file_utils
+import json
+import zipfile
+from craft import CRAFT
+from collections import OrderedDict
+
+
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+
+def str2bool(v):
+    return v.lower() in ("yes", "y", "true", "t", "1")
+
+parser = argparse.ArgumentParser(description='CRAFT Text Detection')
+parser.add_argument('--trained_model', default='weights/craft_mlt_25k.pth', type=str, help='pretrained model')
+parser.add_argument('--text_threshold', default=0.7, type=float, help='text confidence threshold')
+parser.add_argument('--low_text', default=0.4, type=float, help='text low-bound score')
+parser.add_argument('--link_threshold', default=0.4, type=float, help='link confidence threshold')
+parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda for inference')
+parser.add_argument('--canvas_size', default=1280, type=int, help='image size for inference')
+parser.add_argument('--mag_ratio', default=1.5, type=float, help='image magnification ratio')
+parser.add_argument('--poly', default=False, action='store_true', help='enable polygon type')
+parser.add_argument('--show_time', default=False, action='store_true', help='show processing time')
+parser.add_argument('--test_folder', default='/data/', type=str, help='folder path to input images')
+parser.add_argument('--refine', default=False, action='store_true', help='enable link refiner')
+parser.add_argument('--refiner_model', default='weights/craft_refiner_CTW1500.pth', type=str, help='pretrained refiner model')
+parser.add_argument('--image_folder', default='lol', help='path to image_folder which contains text images')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
+parser.add_argument('--saved_model', help="path to saved_model to evaluation", default='weights/TPS-ResNet-BiLSTM-Attn.pth')
+""" Data processing """
+parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
+parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
+parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
+parser.add_argument('--rgb', action='store_true', help='use rgb input')
+parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
+parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
+parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
+""" Model Architecture """
+parser.add_argument('--Transformation', type=str, help='Transformation stage. None|TPS', default='TPS')
+parser.add_argument('--FeatureExtraction', type=str, help='FeatureExtraction stage. VGG|RCNN|ResNet', default='ResNet')
+parser.add_argument('--SequenceModeling', type=str, help='SequenceModeling stage. None|BiLSTM', default='BiLSTM')
+parser.add_argument('--Prediction', type=str, help='Prediction stage. CTC|Attn', default='Attn')
+parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
+parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
+parser.add_argument('--output_channel', type=int, default=512,
+                    help='the number of output channel of Feature extractor')
+parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
+args = parser.parse_args()
+
+
+""" For test images in a folder """
+#image_list, _, _ = file_utils.get_files(args.test_folder)
+
+
+def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None):
+    t0 = time.time()
+
+    # resize
+    img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, args.canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=args.mag_ratio)
+    ratio_h = ratio_w = 1 / target_ratio
+
+    # preprocessing
+    x = imgproc.normalizeMeanVariance(img_resized)
+    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    if cuda:
+        x = x.cuda()
+
+    # forward pass
+    with torch.no_grad():
+        y, feature = net(x)
+
+    # make score and link map
+    score_text = y[0,:,:,0].cpu().data.numpy()
+    score_link = y[0,:,:,1].cpu().data.numpy()
+
+    # refine link
+    if refine_net is not None:
+        with torch.no_grad():
+            y_refiner = refine_net(y, feature)
+        score_link = y_refiner[0,:,:,0].cpu().data.numpy()
+
+    t0 = time.time() - t0
+    t1 = time.time()
+
+    # Post-processing
+    boxes, polys = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+    # coordinate adjustment
+    boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+    polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+    for k in range(len(polys)):
+        if polys[k] is None: polys[k] = boxes[k]
+
+    t1 = time.time() - t1
+
+    # render results (optional)
+    render_img = score_text.copy()
+    render_img = np.hstack((render_img, score_link))
+    ret_score_text = imgproc.cvt2HeatmapImg(render_img)
+
+    if args.show_time : print("\ninfer/postproc time : {:.3f}/{:.3f}".format(t0, t1))
+
+    return boxes, polys, ret_score_text
+
+
+
+if __name__ == '__main__':
+    # load net
+    net = CRAFT()     # initialize
+
+    print('Loading weights from checkpoint (' + args.trained_model + ')')
+    if args.cuda:
+        net.load_state_dict(copyStateDict(torch.load(args.trained_model)))
+    else:
+        net.load_state_dict(copyStateDict(torch.load(args.trained_model, map_location='cpu')))
+
+    if args.cuda:
+        net = net.cuda()
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = False
+
+    net.eval()
+
+    # LinkRefiner
+    refine_net = None
+    if args.refine:
+        from refinenet import RefineNet
+        refine_net = RefineNet()
+        print('Loading weights of refiner from checkpoint (' + args.refiner_model + ')')
+        if args.cuda:
+            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model)))
+            refine_net = refine_net.cuda()
+            refine_net = torch.nn.DataParallel(refine_net)
+        else:
+            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model, map_location='cpu')))
+
+        refine_net.eval()
+        args.poly = True
+
+    t = time.time()
+
+    # load data
+    dataframe = pd.DataFrame(columns=['name', 'characters'])
+
+    for dirpath, dirnames, filenames in os.walk(args.test_folder):
+        if filenames:
+            folder_name = os.path.split(dirpath)[-1]
+            k=0
+            for filename in filenames:
+                k += 1
+                image_path = os.path.join(dirpath, filename)
+                print("Test image {:d}/{:d}: {:s}".format(k, len(filenames), image_path), end='\r')
+                image = imgproc.loadImage(image_path)
+
+                bboxes, polys, score_text = test_net(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly, refine_net)
+
+                text, name = file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder, args=args)
+                df = pd.DataFrame(np.array([[folder_name + '_' + str(name), text]]), columns=['name', 'characters'])
+                dataframe = dataframe.append(df, ignore_index=False)
+
+    dataframe.to_csv('out.csv', index=False)
+    print(dataframe)
+    print("elapsed time : {}s".format(time.time() - t))
